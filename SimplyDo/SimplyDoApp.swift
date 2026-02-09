b826f3c54803c600471ad70082b0c8f27f01f6c5
oleks4ndr <152318987+oleks4ndr@users.noreply.github.com>
2026-02-09 19:45:13 +0400

    Initial Commit

---
+++ SimplyDo/SimplyDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyDoApp.swift
+//  SimplyDo
+//
+//  Created by Oleksandr Anyshchenko on 09/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct SimplyDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
